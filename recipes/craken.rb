e1ef632cbe19254a4f02c0a20cf9ced90e3a7599
retr0h <john@dewey.ws>
2009-05-21 21:56:26 -0700

    added uninstall cap task

--- recipes/craken.rb
+++ recipes/craken.rb
@@ -6,4 +6,9 @@ namespace :craken do
     set :env_args, (exists?(:env_args) ? env_args : "app_name=#{application} deploy_path=#{current_path}")
     run "cd #{current_path} && rake #{env_args} RAILS_ENV=#{rails_env} craken:install"
   end
+
+  desc "Uninstall raketab"
+  task :uninstall, :roles => :cron do
+    run "cd #{current_path} && rake craken:uninstall"
+  end
 end
